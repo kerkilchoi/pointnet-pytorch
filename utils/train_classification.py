cb141a58d334bd02767ab86d8e18cdcac15379b4
Kerkil Choi <kerkil@gmail.com>
2023-01-17 21:29:51 -0800

    Add mps compatibility for the model

--- utils/train_classification.py
+++ utils/train_classification.py
@@ -90,59 +90,63 @@ classifier = PointNetCls(k=num_classes, feature_transform=opt.feature_transform)
 if opt.model != '':
     classifier.load_state_dict(torch.load(opt.model))
 
+device = "mps" if torch.backends.mps.is_available() else "cpu"
 
 optimizer = optim.Adam(classifier.parameters(), lr=0.001, betas=(0.9, 0.999))
 scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
-classifier.cuda()
+classifier.to(device)
 
 num_batch = len(dataset) / opt.batchSize
 
-for epoch in range(opt.nepoch):
-    scheduler.step()
-    for i, data in enumerate(dataloader, 0):
+if __name__ == '__main__':
+    for epoch in range(opt.nepoch):
+        scheduler.step()
+        for i, data in enumerate(dataloader, 0):
+            points, target = data
+            target = target[:, 0]
+            # print("point shape 1, ", points.shape)
+            points = points.transpose(2, 1)
+            # print("point shape 2, ",points.shape)
+            points, target = points.to(device), target.to(device)
+            optimizer.zero_grad()
+            classifier = classifier.train()
+            pred, trans, trans_feat = classifier(points)
+            loss = F.nll_loss(pred, target)
+            if opt.feature_transform:
+                loss += feature_transform_regularizer(trans_feat) * 0.001
+            loss.backward()
+            optimizer.step()
+            pred_choice = pred.data.max(1)[1]
+            correct = pred_choice.eq(target.data).cpu().sum()
+            print('[%d: %d/%d] train loss: %f accuracy: %f' % (epoch, i, num_batch, loss.item(), correct.item() / float(opt.batchSize)))
+
+            if i % 10 == 0:
+                j, data = next(enumerate(testdataloader, 0))
+                points, target = data
+                target = target[:, 0]
+                points = points.transpose(2, 1)
+                points, target = points.to(device), target.to(device)
+                classifier = classifier.eval()
+                pred, _, _ = classifier(points)
+                loss = F.nll_loss(pred, target)
+                pred_choice = pred.data.max(1)[1]
+                correct = pred_choice.eq(target.data).cpu().sum()
+                print('[%d: %d/%d] %s loss: %f accuracy: %f' % (epoch, i, num_batch, blue('test'), loss.item(), correct.item()/float(opt.batchSize)))
+
+        torch.save(classifier.state_dict(), '%s/cls_model_%d.pth' % (opt.outf, epoch))
+
+    total_correct = 0
+    total_testset = 0
+    for i,data in tqdm(enumerate(testdataloader, 0)):
         points, target = data
         target = target[:, 0]
         points = points.transpose(2, 1)
-        points, target = points.cuda(), target.cuda()
-        optimizer.zero_grad()
-        classifier = classifier.train()
-        pred, trans, trans_feat = classifier(points)
-        loss = F.nll_loss(pred, target)
-        if opt.feature_transform:
-            loss += feature_transform_regularizer(trans_feat) * 0.001
-        loss.backward()
-        optimizer.step()
+        points, target = points.to(device), target.to(device)
+        classifier = classifier.eval()
+        pred, _, _ = classifier(points)
         pred_choice = pred.data.max(1)[1]
         correct = pred_choice.eq(target.data).cpu().sum()
-        print('[%d: %d/%d] train loss: %f accuracy: %f' % (epoch, i, num_batch, loss.item(), correct.item() / float(opt.batchSize)))
+        total_correct += correct.item()
+        total_testset += points.size()[0]
 
-        if i % 10 == 0:
-            j, data = next(enumerate(testdataloader, 0))
-            points, target = data
-            target = target[:, 0]
-            points = points.transpose(2, 1)
-            points, target = points.cuda(), target.cuda()
-            classifier = classifier.eval()
-            pred, _, _ = classifier(points)
-            loss = F.nll_loss(pred, target)
-            pred_choice = pred.data.max(1)[1]
-            correct = pred_choice.eq(target.data).cpu().sum()
-            print('[%d: %d/%d] %s loss: %f accuracy: %f' % (epoch, i, num_batch, blue('test'), loss.item(), correct.item()/float(opt.batchSize)))
-
-    torch.save(classifier.state_dict(), '%s/cls_model_%d.pth' % (opt.outf, epoch))
-
-total_correct = 0
-total_testset = 0
-for i,data in tqdm(enumerate(testdataloader, 0)):
-    points, target = data
-    target = target[:, 0]
-    points = points.transpose(2, 1)
-    points, target = points.cuda(), target.cuda()
-    classifier = classifier.eval()
-    pred, _, _ = classifier(points)
-    pred_choice = pred.data.max(1)[1]
-    correct = pred_choice.eq(target.data).cpu().sum()
-    total_correct += correct.item()
-    total_testset += points.size()[0]
-
-print("final accuracy {}".format(total_correct / float(total_testset)))
+    print("final accuracy {}".format(total_correct / float(total_testset)))
